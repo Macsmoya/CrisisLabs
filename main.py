9ae0ac1c8c53420e47888b5178bc488c5ce8522c
Macsmoya <61817442+Macsmoya@users.noreply.github.com>
2021-06-23 18:17:54 +1200

    Tried matplotlib

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-
 import socket
+import numpy as np
+import matplotlib.pyplot as plt
 
 serverAddressPort = ("152.67.115.238", 20001)
 bufferSize = 1024
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 UDPClientSocket.sendto(b"connect", serverAddressPort)
- 
-while True:
+
+
+datapoints = []
+timestamps = []
+for x in range (10):
     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
     msg = msgFromServer[0].decode()
+    msg = msg[1:-1].split(', ')
     # Testing
+    data = msg[2:-1]
+    print(data)
+    datapoints.append(msg[2])
+    timestamps.append(x)
+plt.plot(timestamps, datapoints)    
+
+# naming the x axis
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+
+# giving a title to my graph
+plt.title('My first graph!')
+  
+# function to show the plot
+plt.show()
