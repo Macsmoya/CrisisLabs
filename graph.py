f0c935cdef285c6d593fe019d1b35e146f01d65d
Macsmoya <61817442+Macsmoya@users.noreply.github.com>
2021-06-29 10:50:31 +1200

    Stopped title from updating, Removed Dash testing(now app.py) and began to merge main and graph.

--- graph.py
+++ graph.py
@@ -1,8 +1,16 @@
-
 from csv import writer
 import time
 import random
 import math as maths
+import socket
+import numpy as np
+
+serverAddressPort = ("152.67.115.238", 20001)
+bufferSize = 1024
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+UDPClientSocket.sendto(b"connect", serverAddressPort)
+
 
 FILENAME = "data/data.csv"
 # opening the file with w+ mode truncates the file
@@ -18,9 +26,19 @@ def append_list_as_row(file_name, list_of_elem):
         csv_writer = writer(write_obj)
         # Add contents of list as last row in the csv file
         csv_writer.writerow(list_of_elem)
+
 append_list_as_row(FILENAME, [0, 0])
-for x in range(1, 100):
-    time.sleep(.2)
-    append_list_as_row(FILENAME, [x, maths.sin(x)])
+
+for x in range (100):
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    msg = msgFromServer[0].decode()
+    msg = msg[1:-1].split(', ')
+    # Testing
+    data = msg[2:-1]
+    csv_lst = [x for x in enumerate(data)]
+    print(msg)
+    if msg[0] == "'ENN'":
+        append_list_as_row(FILENAME, (i, msg[3]))
+        
     
     
